15672b66064006db6ba51b3bdbe98e2dc9d93ad6
Seth A. Roby <sethroby@mac.com>
2015-07-30 23:31:49 -0700

    React UI

---
+++ javascript/components.js
@@ -0,0 +1,312 @@
+var Episode = React.createClass({displayName: "Episode",
+  render: function() {
+    var p = this.props.path;
+    var w = this.props.weave;
+    
+    var f = Unravel.Index.filament(p);
+    var md = f.metadata(w);
+    var body = f.data(w);
+    
+    return (
+      React.createElement("article", {className: "episode", "data-weave": w}, 
+        React.createElement("div", {className: "main"}, 
+          React.createElement("header", {className: "info"}, 
+            React.createElement("h1", {className: "title"}, md.title), 
+            React.createElement("dl", null, 
+              React.createElement("dt", null, "Season"), React.createElement("dd", {className: "season"}, md.season), 
+              React.createElement("dt", null, "Episode"), React.createElement("dd", {className: "episode"}, md.episode), 
+              React.createElement("dt", null, "Original Airdate:"), React.createElement("dd", {className: "airdate"}, md.airdate), 
+              React.createElement("dt", null, "Director:"), React.createElement("dd", {className: "director"}, md.director)
+            )
+          ), 
+          React.createElement("div", {className: "body", dangerouslySetInnerHTML: {__html: body}})
+        )
+      )
+    );
+  },
+});
+
+var EpisodeSelectorEpisode = React.createClass({displayName: "EpisodeSelectorEpisode",
+  render: function() {
+    var ep = this.props.episode;
+    
+    var klass = 'episode episode-' + ep.id;
+    if (this.props.isCurrent) klass += " current";
+    
+    var md = ep.metadata(Unravel.Index.lastWeaveName());
+    var title = [md.title, md.summary].join("\n\n");
+    
+    return (
+      React.createElement("li", {key: ep.id, onClick: this.selectEpisode, className: klass, title: title}, 
+        React.createElement("a", {href: '#' + ep.id}, 
+          React.createElement("h3", null, 
+            React.createElement("span", {className: "what"}, "e"), 
+            React.createElement("span", {className: "number"}, ep.episode < 10 ? "0" + ep.episode : ep.episode)
+          )
+        )
+      )
+    );
+  },
+  selectEpisode: function(event) {
+    event.preventDefault();
+    
+    var ep = this.props.episode;
+    this.props.whenSelected(ep.id, ep);
+  },
+});
+
+var EpisodeSelectorSeason = React.createClass({displayName: "EpisodeSelectorSeason",
+  render: function() {
+    var $this = this;
+    return (
+      React.createElement("li", {key: this.props.season, className: "season"}, 
+        React.createElement("h2", null, 
+          React.createElement("span", {className: "what"}, "s"), 
+          React.createElement("span", {className: "number"}, "0", this.props.season)
+        ), 
+        React.createElement("ol", {className: "episodes"}, 
+          $.map(this.props.episodes, function (e) {return $this.episodeLi(e)})
+        )
+      )
+    );
+  },
+  episodeLi: function(ep) {
+    var isCurrent = this.props.currentEp && this.props.currentEp.id == ep.id;
+    return (
+      React.createElement(EpisodeSelectorEpisode, {key: ep.id, episode: ep, isCurrent: isCurrent, whenSelected: this.props.whenSelected})
+    );
+  },
+});
+
+var EpisodeSelector = React.createClass({displayName: "EpisodeSelector",
+  seasonLi: function(s, eps) {
+    return (
+      React.createElement(EpisodeSelectorSeason, {key: s, season: s, episodes: eps, currentEp: this.props.currentEp, whenSelected: this.props.whenSelected})
+    );
+  },
+  sortedEpisodes: function() {
+    return this.props.episodes.sort(function(a, b) {
+      return a.id > b.id ? -1 : 1
+    });
+  },
+  episodeListsBySeason: function() {
+    var h = {};
+    $.map(this.sortedEpisodes(), function(ep, ix) {
+      h[ep.season] = h[ep.season] || [];
+      h[ep.season].push(ep);
+    });
+    return h;
+  },
+  render: function() {
+    var $this = this;
+    if (this.props.header) var header = (React.createElement("h2", null, this.props.header));
+    
+    return (
+      React.createElement("nav", {id: this.props.id, className: "episode-guide"}, 
+        header, 
+        React.createElement("ol", {className: "seasons"}, 
+          $.map(this.episodeListsBySeason(), function(episodes, season) {
+            return $this.seasonLi(season, episodes);
+          })
+        )
+      )
+    );
+  }
+});
+
+var EpisodeDisplay = React.createClass({displayName: "EpisodeDisplay",
+  getInitialState: function() {
+    var eps = Unravel.Index.episodes();
+    var lastEp = eps[eps.length - 1];
+    var selectedEp = this.getEpisodeFromFragment() || lastEp;
+    
+    return {
+      currentEpisodeId: selectedEp.id,
+      supportingDocs: {},
+    };
+  },
+  getEpisodeFromFragment: function() {
+    var hash = document.location.hash;
+    var match = hash.match(/^#s(\d+)e(\d+)/);
+    if (!match) return null;
+    return Unravel.Index.episodes(function(ep) {
+      return ep.season == Number(match[1]) && ep.episode == Number(match[2]);
+    })[0];
+  },
+  selectEpisode: function(id) {
+    this.setState({currentEpisodeId: id});
+    $(document).scrollTop(0);
+  },
+  render: function() {
+    var currentEp = Unravel.Index.episode(this.state.currentEpisodeId);
+    return (
+      React.createElement("div", {onClick: this.handleWeaveLink}, 
+        React.createElement(EpisodeSelector, {id: "global-nav", whenSelected: this.selectEpisode, episodes: Unravel.Index.episodes(), currentEp: currentEp}), 
+        React.createElement(Episode, {path: currentEp.path, weave: currentEp.id}), 
+        React.createElement(SupportingDocumentList, {supportingDocs: this.state.supportingDocs[this.state.currentEpisodeId] || []})
+      )
+    )
+  },
+  handleWeaveLink: function(event) {
+    var $this = $(event.target);
+    if (!$this.is('a.weave-link')) return;
+    
+    event.preventDefault();
+    event.stopPropagation();
+  
+    var href = $this.attr('href');
+    var path = href.replace(/^:weave:\//, '');
+    var m = path.match(Unravel.Episode.pathRegex);
+    if (m) {
+      this.selectEpisode(m[1]);
+    } else {
+      this.addSupportingDoc(path, $this);
+    }
+  },
+  addSupportingDoc: function(path, pinnedTo) {
+    var h = this.state.supportingDocs;
+    var k = this.state.currentEpisodeId;
+    var docs = h[k] || (h[k] = []);
+    var doc = docs.find(function(doc) {return doc.path == path});
+    var sel = this.selectorFor(pinnedTo);
+    
+    if (doc) {
+      doc.pinnedToSelector = sel;
+    } else {
+      docs.push(doc = {
+        path: path,
+        pinnedToSelector: sel,
+      });
+    }
+    
+    this.forceUpdate();
+  },
+  selectorFor: function(el) {
+    el = el[0] || el;
+    var parts = [];
+    while (el && el.tagName) {
+      if (el.id) {
+        parts.splice(0, 0, "#" + el.id);
+        el = null;
+      } else {
+        var index = $(el.parentNode).children().index(el) + 1;
+        index = index > 1 ? (':nth-child(' + index + ')') : '';
+        parts.splice(0, 0, el.tagName.toLowerCase() + index);
+        el = el.parentNode;
+      }
+    }
+    return parts.join(" > ");
+  },
+});
+
+var SupportingDocumentList = React.createClass({displayName: "SupportingDocumentList",
+  componentWillMount: function() {
+    $(window).on('resize', this.reposition);
+  },
+  render: function() {
+    var supportingDocs = $.map(this.props.supportingDocs, function(d) {
+      return (React.createElement(SupportingDocument, {key: d.path, path: d.path, pinnedToSelector: d.pinnedToSelector}))
+    });
+  
+    return (
+      React.createElement("section", {id: "supporting-documents"}, 
+        supportingDocs
+      )
+    );
+  },
+  reposition: function() {
+    this.forceUpdate();
+  }
+});
+
+var SupportingDocument = React.createClass({displayName: "SupportingDocument",
+  getInitialState: function() {
+    var pinnedTo = $(this.props.pinnedToSelector);
+    var pinnedToTop = pinnedTo ? pinnedTo.offset().top : 0;
+    return {
+      weave: Unravel.currentWeaveName(),
+      x: 100,
+      y: pinnedToTop - 100,
+      z: ++zTopDoc,
+      rotation: this.randomRotation(),
+      tilt: 90,
+    };
+  },
+  componentDidMount: function() {
+    this.setState({tilt: 0});
+    this.reposition();
+  },
+  componentWillReceiveProps: function(newProps) {
+    this.reposition(newProps.pinnedToSelector);
+    if (newProps.pinnedToSelector != this.props.pinnedToSelector) {
+      this.setState({
+        rotation: this.randomRotation(),
+        z: ++zTopDoc,
+      });
+    }
+  },
+  randomRotation: function() {
+    return ((Math.random() * 6) - 3)
+  },
+  doc: function() {
+    return Unravel.Index.filament(this.props.path);
+  },
+  episodes: function(doc) {
+    doc = doc || this.doc();
+    return $.map(doc.versions(), function(_, id) {return Unravel.Index.episode(id)});
+  },
+  selectVersion: function(id) {
+    this.setState({weave: id});
+  },
+  reposition: function(pinnedToSelector) {
+    pinnedToSelector = pinnedToSelector || this.props.pinnedToSelector;
+    var pinnedTo = $(pinnedToSelector);
+    this.setState({
+      x: 0,
+      y: pinnedTo.offset().top,
+    });
+  },
+  jumpForward: function() {
+    this.setState({z: ++zTopDoc});
+  },
+  render: function() {
+    var path = this.props.path;
+    var weave = this.state.weave;
+
+    var filament = Unravel.Index.filament(path);
+    var body = filament.data(weave);
+    var metadata = filament.metadata(weave);
+    
+    var imgSrc = metadata.image || ("images/generic/" + path.split("/")[0] + ".png");
+    var eps = this.episodes();
+    var currentEp = eps.find(function(ep) {return ep.id == weave});
+    
+    var transform = [
+      'translateY(' + this.state.y + 'px)',
+      'translateX(' + this.state.x + 'px)',
+      'rotate(' + this.state.rotation + 'deg)',
+      'rotateY(' + this.state.tilt + 'deg)',
+    ].join(' ');
+    var style = {transform: transform, WebkitTransform: transform, zIndex: this.state.z}; 
+    
+    var md = ['Status', 'Actor', 'Player'].map(function (key) {
+      return [(React.createElement("dt", null, key)), (React.createElement("dd", null, metadata[key.toLowerCase()]))];
+    });
+    
+    return (
+      React.createElement("article", {className: "supporting-doc", style: style, onClick: this.jumpForward}, 
+        React.createElement("div", {className: "image note"}, 
+          React.createElement("img", {src: imgSrc})
+        ), 
+        React.createElement(EpisodeSelector, {header: "As of", whenSelected: this.selectVersion, episodes: eps, currentEp: currentEp}), 
+        React.createElement("div", {className: "body"}, 
+          React.createElement("h2", null, metadata.title), 
+          React.createElement("dl", {className: "info"}, 
+            md
+          ), 
+          React.createElement("div", {className: "content", dangerouslySetInnerHTML: {__html: body}})
+        )
+      )
+    );
+  }
+});
--- javascript/unravel.js
+++ javascript/unravel.js
@@ -1,253 +1,105 @@
-var zTopDoc = 1000;
-
-function currentWeave() {
-  return $('article.episode').attr('data-weave');
-};
-
-function getFileAtWeave(path, weave) {
-  weave = weave || currentWeave();
-  
-  var index = $(document).data('index');
-  var pathData = index.filaments[path];
-  var atWeave = pathData[weave];
-  while (atWeave.see) {
-    weave = atWeave.see;
-    atWeave = pathData[weave];
-  }
-  return $.Deferred().resolve(atWeave.data, atWeave.metadata, weave, pathData);
-};
-
-function showEpisode(path, weave) {
-  weave = weave || $('#global-nav').data('last-weave-name');
-  
-  getFileAtWeave(path, weave).then(function(data, metadata) {
-    var article = $('article.episode').attr('data-weave', weave);
-    $.each(metadata, function (key, value) { article.find('.info .' + key).text(value) });
-    article.find('.body').html(data);
-    $('.supporting-doc').remove();
-    $(document).scrollTop(0);
-    Nav.setCurrent($('#global-nav'), path);
-  });
-};
-
-function showArticle(path, weave, pinnedTo) {
-  var id = path.replace(/[.].*?$/, '').replace(/[^a-z0-9-]+/ig, '-');
-  var doc = $('#' + id);
-  
-  if (doc.length == 0) {
-    getFileAtWeave(path, weave).then(function(data, metadata, weave, pathData) {
-      var doc = $('<article/>').attr('id', id).attr('class', 'supporting-doc').attr('data-path', path);
-      var imageNote = $('<div class="image note"/>')
-        .appendTo(doc)
-        .append($('<img/>').attr('src', metadata.image || ('images/generic/' + path.split('/')[0] + '.png')));
-      var nav = $('<nav class="episode-guide"/>')
-        .appendTo(doc)
-        .append($("<h2/>").text('As of'));
-      Nav.setVersions(nav, pathData, weave);
-      
-      showArticle.setBody(doc, data, metadata);
-      
-      animateArticle(doc, pinnedTo);
-    });
-  } else {
-    animateArticle(doc, pinnedTo);
-  }
-};
-showArticle.setBody = function(doc, data, metadata) {
-  var body = $('<div class="body"/>')
-    .append($("<h2 class='title'/>").text(metadata.title))
-    .append($("<dl class='info'/>"))
-    .append(data);
-  
-  var info = function (key, value) {
-    if (!value) return;
-    
-    var dl = body.find('dl.info');
-    dl.append($('<dt/>').text(key)).append($('<dd/>').text(value));
-  }
-  info('Status', metadata.status);
-  info('Actor', metadata.actor);
-  info('Player', metadata.player);
-  
-  var oldBody = doc.find('.body');
-  if (oldBody.length) {
-    oldBody.replaceWith(body);
-  } else {
-    body.appendTo(doc);
-  }
-};
-
-function animateArticle(doc, pinnedTo, options) {
-  options = options || {};
-  var keepRotation = options.keepRotation === true;
-  var keepStackPositions = options.keepStackPositions === true;
-  
-  var transformToPositionAt = function(top, right, tilt) {
-    var position = 'translateY(' + top + 'px)';
-    if (right) position = position + ' translateX(' + right + 'px)';
-    if (tilt) position = position + ' rotateY(' + tilt + 'deg)';
-    var degrees = (keepRotation) ? doc.data('degrees') : ((Math.random() * 6) - 3);
-    doc.data('degrees', degrees);
-    var rotation = 'rotate(' + degrees + 'deg)';
-    var t = [position, rotation].join(' ');
-    doc.css({transform: t, webkitTransform: t});
-  };
-  
-  doc.data('pinned-to', pinnedTo);
-  var newHeight = pinnedTo.offset().top;
-  
-  if (doc.parent().length == 0) {
-    transformToPositionAt(newHeight - 100, 600, 90);
-    doc.appendTo($('#supporting-documents'));
-  }
-
-  if (!keepStackPositions) {
-    doc.css({zIndex: ++zTopDoc});
-  }
-  setTimeout(function () {
-    transformToPositionAt(newHeight);
-  }, 100);
-};
-
-var Nav = {
-  addSeason: function(nav, season) {
-    var seasons = nav.find('ol.seasons');
-    if (seasons.length == 0) seasons = $('<ol class="seasons"/>').appendTo(nav);
-    var li = seasons.find('li.season[data-season=' + season + ']');
-    if (li.length) return li;
-    
-    var li = $('<li class="season"/>')
-      .attr('data-season', season);
-    $('<h2/>')
-      .append($('<span class="what"/>').text('s'))
-      .append($('<span class="number"/>').text(season))
-      .appendTo(li);
-    $('<ol class="episodes"/>')
-      .appendTo(li);
-    li.appendTo(seasons);
-    return li;
+var Unravel = {
+  currentWeaveName: function () {
+    return $('article.episode').attr('data-weave') || Unravel.Index.lastWeaveName();
   },
-  addEpisode: function(nav, season, episode) {
-    var seasonEl = Nav.addSeason(nav, season);
-    var li = seasonEl.find('li.episode[data-episode=' + episode + ']');
-    if (li.length) return li;
+  weaveLink: function (event, detail, sender, context) {
+    var $this = $(event.target);
+    if (!$this.is('a.weave-link')) return;
     
-    var li = $('<li class="episode"/>')
-      .attr('data-season', season)
-      .attr('data-episode', episode);
-    $('<h3/>')
-      .append($('<span class="what"/>').text('e'))
-      .append($('<span class="number"/>').text(episode))
-      .appendTo(li);
-    li.appendTo(seasonEl.find('ol.episodes'));
-    return li;
-  },
-  setCurrent: function(nav, season, episode) {
-    nav.find('.current').removeClass('current');
+    event.preventDefault();
     
-    var epMatch = season.match(/s(\d+)e(\d+)/);
-    if (epMatch) {
-      season = epMatch[1];
-      episode = epMatch[2];
-    }
-    
-    var li = nav.find('li[data-season=' + season + '] li[data-episode=' + episode + ']');
-    var see = li.attr('data-see-version');
-    if (see) {
-      Nav.setCurrent(nav, see);
+    var href = $this.attr('href');
+    var path = href.replace(/^:weave:\//, '');
+    var match = path.match(Unravel.Episode.pathRegex);
+    if (match) {
+      context.fire('episode-picked', {episode: match[1]});
     } else {
-      li.addClass('current');
+      context.fire('supporting-document-picked', {path: path, pin: $this});
     }
-  },
-  episodeRegex: /episodes\/s(\d\d)e(\d\d)/,
-  setWeaves: function(nav, weaves) {
-    nav.data('weaves', weaves);
-    nav.data('last-weave-name', weaves[0].name);
-  
-    var episodes = weaves.reduce(function(eps, weave) {
-      return eps.concat(weave.changed.filter(function(c) {
-        return Nav.episodeRegex.test(c);
-      }).map(function (path) {
-        return {path: path, weave: weave.name};
-      }));
-    }, []).filter(function (item, ix, eps) {
-      return eps.findIndex(function(i) { return i.path == item.path}) === ix;
-    }).sort(function (a, b) {
-      return b.path > a.path ? 1 : a.path > b.path ? -1 : 0;
-    });
-  
-    episodes.forEach(function (ep) {
-      var match = ep.path.match(Nav.episodeRegex);
-      if (match) {
-        var li = Nav.addEpisode(nav, match[1], match[2])
-          .attr('data-path', ep.path)
-          .attr('data-weave', ep.weave)
-          .click(function (event) { showEpisode(ep.path, ep.weave); });
-      }
-    });
-    
-    if (nav.attr('id') == 'global-nav') {showEpisode(episodes[0].path, episodes[0].weave);}
-  },
-  setVersions: function(nav, versions, currentVersion) {
-    versions = versions || {};
-    var versionNames = $.map(versions, function(val,key) {return key}).sort(function (a,b) {return a > b ? -1 : 1});
-    nav.data('versions', versionNames);
-    nav.data('versionInfo', versions);
-    
-    $.each(versionNames, function() {
-      var version = this;
-      var versionInfo = versions[version];
-      var match = version.match(/^s(\d+)e(\d+)$/);
-      if (match) {
-        var li = Nav.addEpisode(nav, match[1], match[2])
-          .attr('data-version', version)
-          .attr('data-see-version', versionInfo.see)
-          .click(function (event) {
-            var doc = $(event.target).closest('.supporting-doc');
-            showArticle.setBody(doc, versionInfo.data, versionInfo.metadata);
-            Nav.setCurrent(nav, match[1], match[2]);
-          });
-      }
-    });
-    
-    Nav.setCurrent(nav, currentVersion || versionNames[0]);
-    
-    return nav;
-  },
+  }
 };
 
-$(document).on('click', "a.weave-link", function (event) {
-  event.preventDefault();
-  
-  var $this = $(this);
-  var href = $this.attr('href');
-  var path = href.replace(/^:weave:\//, '');
-  if (/episodes\//.test(path)) {
-    showEpisode(path);
-  } else {
-    showArticle(path, null, $this);
+Unravel.Filament = function(path, versions) {
+  this.path = path;
+  this._versions = versions;
+  $.extend(this, this.metadata());
+};
+$.extend(Unravel.Filament.prototype, {
+  versions: function () {
+    return this._versions = this._versions || Unravel.Index.data().filaments[this.path];
+  },
+  version: function(weave) {
+    weave = weave || Unravel.currentWeaveName();
+    var v = this.versions()[weave];
+    while (v.see) {v = this.versions()[v.see]};
+    return v;
+  },
+  data: function(weave) {
+    return this.version(weave).data;
+  },
+  metadata: function(weave) {
+    return this.version(weave).metadata;
+  },
+  get: function (weave) {
+    return $.Deferred().resolve(this.data(weave), this.metadata(weave), weave, this);
   }
 });
 
-$(document).on('click', '#supporting-documents article', function () {
-  $(this).css({zIndex: zTopDoc++});
-});
+Unravel.Episode = {
+  pathRegex: /episodes\/(s(\d+)e(\d+))/
+};
 
-$(window).on('resize', function () {
-  $('article.supporting-doc').each(function () {
-    var $this = $(this);
-    var pinnedTo = $this.data('pinned-to');
-    animateArticle($this, pinnedTo, {keepRotation: true, keepStackPositions: true});
-  });
-});
+Unravel.Index = {
+  data: function () {
+    if (arguments.length > 0) {
+      return $(document).data('index', arguments[0]);
+    } else {
+      return $(document).data('index');
+    }
+  },
+  lastWeave: function() {
+    return Unravel.Index.data().weaves[0];
+  },
+  lastWeaveName: function() {
+    return Unravel.Index.lastWeave().name;
+  },
+  allFilaments: function(that) {
+    that = that || function(o) {return o};
+    return $.map(Unravel.Index.data().filaments, function(versions, path) {
+      var filament = Unravel.Index.filament(path, versions);
+      if (that(filament)) return filament;
+    });
+  },
+  episodes: function(that) {
+    that = that || function(o) {return o};
+    return Unravel.Index.allFilaments(function(ep) {
+      var match = ep.path.match(Unravel.Episode.pathRegex);
+      if (!match) return null;
+      ep.id = 's' + match[2] + 'e' + match[3];
+      if (that(ep)) return ep;
+    });
+  },
+  episode: function(id) {
+    return Unravel.Index.episodes(function(ep) {return ep.id == id})[0];
+  },
+  filament: function(path, versions) {
+    this._filaments = this._filaments || {};
+    return this._filaments[path] = (this._filaments[path] || new Unravel.Filament(path, versions));
+  }
+};
+
+var zTopDoc = 1000;
 
 $(function () {
   return $.get('index.json').then(function (data) {
     if (typeof(data) == "string") {data = JSON.parse(data);}
     
-    $(document).data('index', data);
+    Unravel.Index.data(data);
     
-    var globalNav = $('#global-nav');
-    Nav.setWeaves(globalNav, data.weaves);
+    React.initializeTouchEvents(true);
+    React.render(
+      React.createElement(EpisodeDisplay),
+      document.querySelector('#app')
+    );
   });
 });
